76aabaa2cfcc8748f7ab57c8fe943f8a3b41bc96
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-06-20 20:41:34 +0300

    Work with authentication. Added sessions

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var mysql = require('mysql');
 var session = require('cookie-session')
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
+var crypto = require('crypto');
 
 nconf.argv()
 	.env()
@@ -19,13 +20,57 @@ var pool = mysql.createPool({
 });
 
 var app = express();
+var publicUrls = [
+	'/login'
+];
+
+var sessions = [];
+
+var checkSession = function( req, res, next ) {
+	var sessionExist = false;
+	if (req.session.sessionKey) {
+		for (var i = 0; i < sessions.length; i++) {
+			if ( sessions[i].id == req.session.sessionKey ) {
+				sessionExist = true;
+				break;
+			}
+		}
+	}
+
+	if ( !sessionExist && (publicUrls.indexOf(req.url) == -1) ) {
+		console.log('key: ' + req.session.sessionKey);
+		req.session.sessionKey = null;
+		res.redirect( 302, '/login.html');
+	} else {
+		next();
+	}
+}
+
 app.use(session({
 	keys : ['secret1', 'secret2']
 }));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(serveStatic('public'));
+app.use(checkSession);
 app.use(serveStatic('webapp'));
 
 
 app.listen(nconf.get('port'), function() {
-	console.log('Server running at ' + nconf.get('port') + 'port');
+	console.log('Server running at ' + nconf.get('port') + ' port');
+});
+
+app.post('/login', function (req, res) {
+	var creds = req.body;
+	var len = 256;
+	var isCredNotReady = true;
+	var sessionKey;
+	while ( isCredNotReady ) {
+		sessionKey = crypto.randomBytes(len).toString('hex');
+		if ( sessions.indexOf(sessionKey) == -1 ) {
+			isCredNotReady = false;
+		}
+	}
+	sessions.push({id : sessionKey});
+	req.session.sessionKey = sessionKey;
+	res.redirect( 302, '/');
 });
