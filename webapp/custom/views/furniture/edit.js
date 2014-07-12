2fdca7b45bd3b6df8ec7bf79aa3ac8da021ba395
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-07-12 19:48:08 +0300

    Start working with adding category to furniture

--- webapp/custom/views/furniture/edit.js
+++ webapp/custom/views/furniture/edit.js
@@ -14,7 +14,8 @@ define([
 		events: {
       		'click .furnitureSaveButton' : 'save'
     	},
-    	furniture : null,
+    furniture : null,
+    ui : null,
 		render : function ( src, callback ) {
       		var view = this;
       		var furniture;
@@ -25,6 +26,7 @@ define([
           			success: function ( result ) {
             			view.furniture = result;
                   $(view.el).html(_.template(contentTemplate, {furniture: result.toJSON()}));
+                  view.initUiComponents( src, callback );
           			},
           			error: function () {
           				console.log('error!');
@@ -33,8 +35,35 @@ define([
       		} else {
         		view.furniture = new Furniture();
         		$(view.el).html(_.template(contentTemplate, {furniture: view.furniture.toJSON()}));
+            view.initUiComponents( src, callback );
       		}
 		},
+
+    initUiComponents : function ( src,  callback ) {
+      var view = this;
+
+      view.ui.categoryTree = new webix.ui({
+        view : "datatable",
+      });
+
+      view.ui.categoryTreeWen = new webix.ui({
+        view : "window",
+        position : "center",
+        move : true,
+        id : "addCategoryTreeWen",
+        head : "Add Category",
+        body : {
+          template : "Save text"
+        },
+        modal : true
+      });
+
+
+
+      if ( callback ) {
+        callback();
+      }
+    },
 
 		save : function () {
       		var furniture = this.furniture;
