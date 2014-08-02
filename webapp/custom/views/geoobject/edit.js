75e8ab1b857d1ea361e864431556f4dc76ec17fb
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-08-02 19:57:43 +0300

    Update for geoobject edit page

--- webapp/custom/views/geoobject/edit.js
+++ webapp/custom/views/geoobject/edit.js
@@ -4,15 +4,28 @@ define([
   'backbone',
   'events',
   'async',
-  'collections/geoobjects',
+  'models/geoobject',
   'text!custom/templates/geoobject/edit.html'
-], function ($, _, Backbone, Events, Async, Geoobjects, contentTemplate) {
+], function ($, _, Backbone, Events, Async, Geoobject, contentTemplate) {
 	var ContentView = Backbone.View.extend({
 		el : '.content',
 		template : contentTemplate,
 		render : function ( src, callback ) {
 			var view = this;
-			$(view.el).html(_.template(contentTemplate));
+			if ( src.id != -1 ) {
+				var geoobject = new Geoobject({ id: src.id });
+				geoobject.fetch({
+					success: function ( result ) {
+						var geoobjectVar = result.toJSON();
+						$(view.el).html(_.template(contentTemplate, { geoobject : geoobjectVar }));	
+					},
+					error : function ( err ) {
+						console.log( err );
+					}
+				});
+			} else {
+				$(view.el).html(_.template(contentTemplate, { geoobject : {} }));	
+			}
 		}
 	});
 	return ContentView;
