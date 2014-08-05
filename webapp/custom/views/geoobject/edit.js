6b5b1c46e0fb7466a30c0dd48900b5f0c1f68ccb
Kiryl Parfiankou <Kiryl_Parfiankou@epam.com>
2014-08-05 15:31:20 +0300

    Finished with goobjec edit page

--- webapp/custom/views/geoobject/edit.js
+++ webapp/custom/views/geoobject/edit.js
@@ -13,8 +13,11 @@ define([
 		geoobject : null,
 		map : null,
 		placemark : null,
+		placemarkChecked : false,
 		events : {
-			'click .goobjectSaveButton' : 'save'
+			'click .goobjectSaveButton' : 'save',
+			'click #placemarkCheckBtn' : 'checkPlacemark',
+			'click #placemarkUncheckBtn' : 'uncheckPlacemark',
 		},
 		render : function ( src, callback ) {
 			var view = this;
@@ -23,7 +26,13 @@ define([
 				view.geoobject.fetch({
 					success : function ( result ) {
 						var geoobjectVar = result.toJSON();
-						$(view.el).html(_.template(contentTemplate, { geoobject : geoobjectVar }));
+						console.log('geoobjectVar.latitude ' + geoobjectVar.latitude);
+						console.log('geoobjectVar.longitude ' + geoobjectVar.longitude);
+						if ( geoobjectVar.latitude && geoobjectVar.longitude ) {
+							console.log('cheked!');
+							view.placemarkChecked = true;
+						}
+						$(view.el).html(_.template(contentTemplate, { geoobject : geoobjectVar, placemarkChecked : view.placemarkChecked }));
 						view.renderMap();
 					}, 
 					error : function ( err ) {
@@ -32,7 +41,7 @@ define([
 				});
 			} else {
 				view.geoobject = new Geoobject();
-				$(view.el).html(_.template(contentTemplate, { geoobject : {} }));	
+				$(view.el).html(_.template(contentTemplate, { geoobject : {}, placemarkChecked : view.placemarkChecked }));
 			}
 		},
 
@@ -41,7 +50,9 @@ define([
 			view.geoobject.save({				
 				label : $('#geoobjectLabel').val(),
 				mark : $('#geoobjectMark').val(),
-				address : $('#geoobjectAddres').val()
+				address : $('#geoobjectAddres').val(),
+				latitude : view.geoobject.latitude,
+				longitude : view.geoobject.longitude
 			}, {
 				success : function( geoobject ) {
 					window.location.hash = '/view/geoobject.list';
@@ -51,21 +62,47 @@ define([
 
 		renderMap : function() {
 			var view = this;
+
 			view.map = new ymaps.Map("geoobjectMap", {
 				center: [55.76, 37.64], 
-				zoom: 7
+				zoom: 14
 			});
-			console.log('with preset!');
+
+			var geoobjectVar = view.geoobject.toJSON();
+			if ( geoobjectVar.latitude && geoobjectVar.longitude ) {
+				view.placemark = new ymaps.Placemark( [geoobjectVar.latitude, geoobjectVar.longitude], {}, {preset : 'islands#darkGreenDotIcon'} );
+				view.map.geoObjects.add( view.placemark );
+				view.map.setCenter( [geoobjectVar.latitude, geoobjectVar.longitude] );
+			}
+
 			view.map.events.add('click', function(e) {
-				console.log('with preset');
-				var pointGeometry = new ymaps.geometry.Point(e.get('coords'));
-				if ( !view.placemark ) {
+				var coords = e.get('coords');
+				var pointGeometry = new ymaps.geometry.Point(coords);
+				if ( !view.placemark && view.placemarkChecked ) {
 					view.placemark = new ymaps.Placemark( pointGeometry, {}, {preset : 'islands#darkGreenDotIcon'} );
 					view.map.geoObjects.add( view.placemark );
-				} else {
-					view.placemark.geometry.setCoordinates(e.get('coords'));
+					view.geoobject.latitude = coords[0];
+					view.geoobject.longitude = coords[1];
 				}
 			});
+		},
+
+		checkPlacemark : function() {
+			var view = this;
+			$('#placemarkCheckBtn').addClass('hidden');
+			$('#placemarkUncheckBtn').removeClass('hidden');
+			view.placemarkChecked = true;
+		},
+
+		uncheckPlacemark : function() {
+			var view = this;
+			$('#placemarkUncheckBtn').addClass('hidden');
+			$('#placemarkCheckBtn').removeClass('hidden');
+			view.placemarkChecked = false;
+			if ( view.map ) {
+				view.map.geoObjects.remove( view.placemark );
+				view.placemark = null;
+			}
 		}
 	});
 	return ContentView;
