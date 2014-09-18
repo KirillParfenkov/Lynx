5d8091114ae585439c55bb411f7e7ca16f9a8665
Kiryl Parfiankou <Kirill.Parfenkov@gmail.com>
2014-09-19 00:07:39 +0200

    Update for user list page

--- webapp/js/views/setup/usersView.js
+++ webapp/js/views/setup/usersView.js
@@ -1,55 +1,63 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'events',
-  'libs/queue/queue',
-  'models/user',
-  'collections/users',
-  'text!templates/setup/usersView.html' 
-], function ($, _, Backbone, Events, Queue, User, Usres, usersViewTemplate) {
+    'jquery',
+    'underscore',
+    'backbone',
+    'underi18n',
+    'libs/queue/queue',
+    'models/user',
+    'collections/users',
+    'text!templates/setup/user/list/list.html',
+    'less!templates/setup/user/list/list.less'
+], function ($, _, Backbone, underi18n, Queue, User, Usres, usersViewTemplate) {
 	var UsersView = Backbone.View.extend({
-		el : '.content',
-    initialize: function () {
-      this.el = '.content';
-    },
     users : new Usres(),
 		render : function ( src, callback ) {
 			var view = this;
-      var queue = new Queue([
-        function(queue) {
-          if ( view.users ) {
-            view.users = new Usres();
-            view.users.fetch({
-              success : function () {
-                queue.next();
-              },
-              error : function() {
-                console.log('error!');
-                queue.next();
-              }
-            });
-          }
-        },
-        function(queue) {
-          $(view.el).html(_.template(usersViewTemplate, {users: view.users.toJSON()}));
-          if ( callback ) {
-            callback();
-          }
-        }]);
-      queue.start();
+            var queue = new Queue([
+            function(queue) {
+                if ( view.users ) {
+                    view.users = new Usres();
+                    view.users.fetch({
+                        success : function () {
+                            queue.next();
+                        },
+                        error : function( err ) {
+                            console.log( err );
+                            queue.next();
+                        }
+                    });
+                }
+            },
+            function(queue) {
+                $(view.el).html(_.template( underi18n.template(usersViewTemplate, view.i18n), {users: view.users.toJSON()}));
+                if ( callback ) {
+                    callback();
+                }
+            }]);
+            queue.start();
 		},
 
-    hasPermission : function( systemPermissionSet ) {
-      if ( systemPermissionSet && systemPermissionSet.allowEditUsers ) {
-        if ( systemPermissionSet.allowEditUsers.indexOf('read') == -1 ) {
-          return false;
+        hasPermission : function( systemPermissionSet ) {
+            if ( systemPermissionSet && systemPermissionSet.allowEditUsers ) {
+                if ( systemPermissionSet.allowEditUsers.indexOf('read') == -1 ) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            return true;
+        },
+
+        loadI18n : function ( i18n, done ) {
+            var path = '/templates/setup/user/list/';
+            var view = this;
+            $.get( path + i18n + '.json', function( data ) {
+                view.i18n = underi18n.MessageFactory( data );
+                done( null, view.i18n );
+            }).fail( function( err ) {
+                done( err );
+            });
         }
-      } else {
-        return false;
-      }
-      return true;
-    }
-	});
+    });
 	return UsersView;
 });
