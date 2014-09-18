0444bbae306c571b7c3fdca1d737f77dd4675f50
Kiryl Parfiankou <Kiryl_Parfiankou@epam.com>
2014-09-18 12:19:37 +0300

    Update for tab list page

--- webapp/js/views/setup/tabsView.js
+++ webapp/js/views/setup/tabsView.js
@@ -1,49 +1,61 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'events',
-  'libs/queue/queue',
-  'models/tab',
-  'collections/tabs',
-  'text!templates/setup/tabsView.html' 
-], function ($, _, Backbone, Events, Queue, Tab, Tabs, tabsViewTemplate) {
+    'jquery',
+    'underscore',
+    'backbone',
+    'events',
+    'libs/queue/queue',
+    'models/tab',
+    'collections/tabs',
+    'text!templates/setup/tab/list/list.html',
+    'less!templates/setup/tab/list/list.less'
+], function ($, _, Backbone, Events, Queue, Tab, Tabs, template) {
 	var TabsView = Backbone.View.extend({
 		render : function ( src, callback ) {
-      var view = this;
-      var tabs = null;
-      var queue = new Queue([
-        function(queue) {
-            tabs = new Tabs();
-            tabs.fetch({
-              success : function () {
-                queue.next();
-              },
-              error : function() {
-                console.log('error!');
-                queue.next();
-              }
-            });
-        },
-        function(queue) {
-          $(view.el).html(_.template(tabsViewTemplate, {tabs: tabs.toJSON()}));
-          if ( callback ) {
-            callback();
-          }
-        }]);
-      queue.start();
+            var view = this;
+            var tabs = null;
+            var queue = new Queue([
+                function(queue) {
+                    tabs = new Tabs();
+                    tabs.fetch({
+                        success : function () {
+                            queue.next();
+                        },
+                         error : function( err ) {
+                            console.log( err );
+                            queue.next();
+                        }
+                    });
+                },
+                function(queue) {
+                    $(view.el).html(_.template( underi18n.template(template, view.i18n), {tabs: tabs.toJSON()}));
+                    if ( callback ) {
+                        callback();
+                    }
+                }]);
+            queue.start();
 		},
 
-    hasPermission : function( systemPermissionSet ) {
-      if ( systemPermissionSet && systemPermissionSet.allowEditTabs ) {
-        if ( systemPermissionSet.allowEditTabs.indexOf('read') == -1 ) {
-          return false;
+        hasPermission : function( systemPermissionSet ) {
+            if ( systemPermissionSet && systemPermissionSet.allowEditTabs ) {
+                if ( systemPermissionSet.allowEditTabs.indexOf('read') == -1 ) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+            return true;
+        },
+
+        loadI18n : function ( i18n, done ) {
+            var path = '/templates/setup/tab/list/';
+            var view = this;
+            $.get( path + i18n + '.json', function( data ) {
+                view.i18n = underi18n.MessageFactory( data );
+                done( null, view.i18n );
+            }).fail( function( err ) {
+                done( err );
+            });
         }
-      } else {
-        return false;
-      }
-      return true;
-    }
-	});
+    });
 	return TabsView;
 });
--- webapp/js/views/setup/usersView.js
+++ webapp/js/views/setup/usersView.js
@@ -1,55 +1,51 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'events',
-  'libs/queue/queue',
-  'models/user',
-  'collections/users',
-  'text!templates/setup/usersView.html' 
-], function ($, _, Backbone, Events, Queue, User, Usres, usersViewTemplate) {
+    'jquery',
+    'underscore',
+    'backbone',
+    'underi18n',
+    'libs/queue/queue',
+    'models/user',
+    'collections/users',
+    'text!templates/setup/usersView.html'
+], function ($, _, Backbone, underi18n, Queue, User, Usres, usersViewTemplate) {
 	var UsersView = Backbone.View.extend({
-		el : '.content',
-    initialize: function () {
-      this.el = '.content';
-    },
-    users : new Usres(),
+        users : new Usres(),
 		render : function ( src, callback ) {
 			var view = this;
-      var queue = new Queue([
-        function(queue) {
-          if ( view.users ) {
-            view.users = new Usres();
-            view.users.fetch({
-              success : function () {
-                queue.next();
-              },
-              error : function() {
-                console.log('error!');
-                queue.next();
+            var queue = new Queue([
+            function(queue) {
+                if ( view.users ) {
+                    view.users = new Usres();
+                    view.users.fetch({
+                        success : function () {
+                            queue.next();
+                        },
+                        error : function( err ) {
+                            console.log( err );
+                            queue.next();
+                        }
+                    });
+                }
+            },
+            function(queue) {
+              $(view.el).html(_.template(usersViewTemplate, {users: view.users.toJSON()}));
+              if ( callback ) {
+                callback();
               }
-            });
-          }
-        },
-        function(queue) {
-          $(view.el).html(_.template(usersViewTemplate, {users: view.users.toJSON()}));
-          if ( callback ) {
-            callback();
-          }
-        }]);
-      queue.start();
+            }]);
+            queue.start();
 		},
 
-    hasPermission : function( systemPermissionSet ) {
-      if ( systemPermissionSet && systemPermissionSet.allowEditUsers ) {
-        if ( systemPermissionSet.allowEditUsers.indexOf('read') == -1 ) {
-          return false;
+        hasPermission : function( systemPermissionSet ) {
+            if ( systemPermissionSet && systemPermissionSet.allowEditUsers ) {
+                if ( systemPermissionSet.allowEditUsers.indexOf('read') == -1 ) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+          return true;
         }
-      } else {
-        return false;
-      }
-      return true;
-    }
 	});
 	return UsersView;
 });
